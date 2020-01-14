c0448865d015df13af3acf6fb9bbc175686799d3
Bysokar <draksterau@gmail.com>
2020-01-14 18:32:01 +1100

    Should fix stuff

--- src/main/java/me/kyllian/translator/listeners/PlayerJoinListener.java
+++ src/main/java/me/kyllian/translator/listeners/PlayerJoinListener.java
@@ -1,5 +1,7 @@
 package me.kyllian.translator.listeners;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import me.kyllian.translator.TranslatorPlugin;
 import me.kyllian.translator.utils.Language;
 import me.kyllian.translator.utils.PlayerData;
@@ -19,14 +21,31 @@ public class PlayerJoinListener implements Listener {
         this.plugin = plugin;
         Bukkit.getPluginManager().registerEvents(this, plugin);
     }
-
+    public String getLanguage(Player p){
+        try {
+            Object ep = getMethod("getHandle", p.getClass()).invoke(p, (Object[]) null);
+            Field f = ep.getClass().getDeclaredField("locale");
+            f.setAccessible(true);
+            String language = (String) f.get(ep);
+            return language;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+    private Method getMethod(String name, Class<?> clazz) {
+        for (Method m : clazz.getDeclaredMethods()) {
+            if (m.getName().equals(name)) return m;
+        }
+        return null;
+    }
     @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
     public void onPlayerJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
         PlayerData playerData = plugin.getPlayerData(player.getUniqueId());
         //if (player.hasPermission("autocast.update") && plugin.isUpdateCheck()) player.sendMessage(plugin.getUpdateChecker().getUpdateMessage());
-        String playerLanguage = player.getLocale();
-        String language = plugin.getDataHandler().getData().getString(player.getUniqueId().toString() + ".language", playerLanguage);
+        String playerLanguage = getLanguage(player);
+        boolean determinedLanguage = (playerLanguage != null);
+        String language = plugin.getDataHandler().getData().getString(player.getUniqueId().toString() + ".language", (determinedLanguage ? playerLanguage : "en"));
         try {            
             playerData.setLanguage(Language.valueOf(language));
             plugin.getDataHandler().getData().set(player.getUniqueId().toString() + ".language", language); // Default to locale.            
